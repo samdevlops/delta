86358f1cd79feccf2055c01205dc9bc6504a98b2
Saurabh <saurabhmishra6179@gmail.com>
2022-02-03 10:50:27 +0530

    Added styling and Members Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import LoginForm from './components/loginComponent';
 import Navbar from './components/navbarComponent';
 import SignUp from './components/signupComponent';
+import Members from './components/membersComponent';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <SignUp />
+      <div className='content'>
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/members" component={Members} />
+          <Route path="/" component={LoginForm} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/components/membersComponent.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Members extends Component {
+    state = {  } 
+    render() { 
+        return (<h1>Members Component</h1>);
+    }
+}
+ 
+export default Members;
